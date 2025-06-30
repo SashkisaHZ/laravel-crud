7132cbabee9340e097d6ad06abfe2b359379617a
Alexander Atanasov <atan0007@hz.nl>
2025-06-30 02:32:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('home');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/', [PostController::class, 'index'])->name('home');  // protected home page
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
